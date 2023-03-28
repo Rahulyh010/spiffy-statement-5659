6cb51167e1fa7410e55d011b5caf50814244d9d8
gulam160 <gulam.mu159@gmail.com>
2023-03-28 15:24:59 +0530

    My first commit

--- india_mart/src/App.js
+++ india_mart/src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>India Mart</h1>
+      <i
+        className="fa fa-shopping-cart"
+        style={{ fontSize: "48px", color: "#00a699", marginTop: "3rem" }}
+      ></i>
+      <h1>India Mart</h1>
+      <p>DigiMART - An Online Market Place For All Your Need</p>
     </div>
   );
 }
