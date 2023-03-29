f26bf4ce2067bd16a5541b118ab07c9f43ad5870
Rahulyh010 <110051547+Rahulyh010@users.noreply.github.com>
2023-03-29 15:08:57 +0530

    push

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
 const express= require("express");
 const { db } = require("./db");
+const { userRouter } = require("./Routes/user.route");
 
 const app=express();
 
 
 app.use(express.json())
 
-
+app.use("/users",userRouter)
 
 app.listen(8080,db)
