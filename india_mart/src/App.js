136f31e68c8255dc046090679fd4df5ee5324a70
gulam160 <gulam.mu159@gmail.com>
2023-03-29 23:46:59 +0530

    Navbar added

--- india_mart/src/App.js
+++ india_mart/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navabr from "./components/navbar";
+import Allroutes from "./components/Allroutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Allroutes />
+      <Navabr />
+    </div>
+  );
 }
 
 export default App;
