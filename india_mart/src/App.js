5454b4cda78b75b80327aae8bbecb9ef421e0373
Mehul-Kanjariya <mehulktik@gmail.com>
2023-03-30 10:33:50 +0530

    Productlist

--- india_mart/src/App.js
+++ india_mart/src/App.js
@@ -1,16 +1,14 @@
-import AdminRoutes from "./Admin/AdminRoutes/Routes";
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      {/* <i
+      <i
         className="fa fa-shopping-cart"
         style={{ fontSize: "48px", color: "#00a699", marginTop: "3rem" }}
       ></i>
       <h1>India Mart</h1>
-      <p>DigiMART - An Online Market Place For All Your Need</p> */}
-      <AdminRoutes/>
+      <p>DigiMART - An Online Market Place For All Your Need</p>
     </div>
   );
 }
