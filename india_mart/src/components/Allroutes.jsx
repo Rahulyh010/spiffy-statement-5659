7169ab08c373ede54b5bcfa7182e59c9f30a042d
Mehul-Kanjariya <mehulktik@gmail.com>
2023-04-01 00:23:48 +0530

    CRUD complete from admin side

--- india_mart/src/components/Allroutes.jsx
+++ india_mart/src/components/Allroutes.jsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import { ProductsPage } from "../Pages/ProductsPage";
 import { ProductCard } from "./productCard";
+import Productlist from "../Admin/Pages/Productlist";
+import About from "../Admin/Pages/About";
+import AddProduct from "../Admin/Pages/AddProduct";
+import Updateproduct from "../Admin/Pages/Updateproduct";
 
 const Allroutes = () => {
   return <div>
   <Routes>
     <Route path="/productPage" element={<ProductsPage/>}/>
+    <Route path="/adminAbout" element={<About/>}/>
+    <Route path="/admin" element={<Productlist/>}/>
+    <Route path="/adminAddProduct" element={<AddProduct/>}/>
+    <Route path="/adminUpdateProduct/:id" element={<Updateproduct/>}/>
   </Routes>
   </div>;
 };
